4e199ff6d38d9b5b1a539686eddb8a90951526ee
Andrea Hernandez <abeherham@knights.ucf.edu>
2023-04-13 11:37:29 -0400

    Made url secure

--- src/pages/components/connectToRobot.jsx
+++ src/pages/components/connectToRobot.jsx
@@ -7,7 +7,7 @@ export default function ConnectToRobot() {
     const Connect = () => {
         setRos(
             new ROSLIB.Ros({
-              url : 'ws:///6.tcp.ngrok.io:17975'
+              url : 'wss:///6.tcp.ngrok.io:17975'
             }
           ));
     }
